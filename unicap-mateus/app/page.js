67cdb467bb2b1d241d4d6cafb5d831d8a62e018e
Mateus F. Moura <mf753161@gmail.com>
2026-08-18 15:15:02 +0000

    Home page

--- unicap-mateus/app/page.js
+++ unicap-mateus/app/page.js
@@ -1,21 +1,14 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import Presentation from "../components/Presentation";
-import Experience from "../components/Experience";
+import Perfil from "../components/Perfil";
+import Minibio from "../components/Minibio";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Presentation />
-        <Image src="/perfil.jpg"
-          alt="Foto de Mateus Ferreira de Moura"
-          width={150}
-          height={150}
-          style={{ borderRadius: '50%', marginBottom: '1rem' }}
-        />
-        <Experience />
-      </main>
-    </div>
+    <main className={styles.page}>
+      <section className={styles.card}>
+        <Perfil />
+        <Minibio />
+      </section>
+    </main>
   );
 }
