d6710d9578f9f2898404d00076899994e0e7af28
Mateus F. Moura <mf753161@gmail.com>
2026-08-18 15:07:38 +0000

    Removendo testes antigos

--- unicap-mateus/components/Presentation.jsx
+++
@@ -1,9 +0,0 @@
-export default function Presentation() {
-    return (
-        <section>
-            <h1>Mateus Ferreira de Moura</h1>
-            <p>Desenvolvedor Full Stack com foco em Back-End, especializado em Java e Spring Boot. 
-                Estudante de Sistemas para Internet na UNICAP pelo programa Embarque Digital</p>
-        </section>
-    );
-}
